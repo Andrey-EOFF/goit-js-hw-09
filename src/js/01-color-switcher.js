fc458f7d05a47c8c1d377158298a74226340f288
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-03-19 15:24:48 +0200

    task-1-2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    const newBgColor = getRandomHexColor();
+    document.body.style.backgroundColor = newBgColor;
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
+});
